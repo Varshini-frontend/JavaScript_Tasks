40e433d3a0356c7d6022eb461d6d1c924ca99eed
Varshini-frontend <varshinianandaraj@gmail.com>
2024-07-28 20:39:42 +0530

    Ch5_pgm_07.js

--- 05_JS_Arguments/Ch5_pgm_07.js
+++ 05_JS_Arguments/Ch5_pgm_07.js
@@ -2,13 +2,23 @@
 
 var showPlayerName;
 
+var showPlayerName;
+
 showPlayerName = function (playerName) {
-    console.log(playerName);
+    // console.log(playerName);
+    console.log("The player's name is " + playerName);
 };
 
 showPlayerName("Kandra");
 showPlayerName("Dax");
 
+
+var playerNameLength;
+playerNameLength = function(playerName){
+    console.log("The number of letters in the: " + playerName.length);
+}
+playerNameLength("Kandra");
+playerNameLength("KGCAS");
 
 
 /* Further Adventures
