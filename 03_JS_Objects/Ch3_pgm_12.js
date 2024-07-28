d5a408ab73f36aecf2f3206a059a024443a2bee7
Varshini-frontend <varshinianandaraj@gmail.com>
2024-07-28 20:19:00 +0530

    Ch3_pgm_12.js

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -1,5 +1,7 @@
 // A blog post
 
+const { title } = require("process");
+
 var post = {
 	id : 1,
 	title : "My Crazy Space Adventure",
@@ -7,6 +9,17 @@ var post = {
 	created : "2015-06-21",
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
+console.log(post.author);
+console.log(post.id);
+console.log(post.title);
+
+var post2 ={
+	id : 2,
+	title : "Personal Bio" ,
+	created : "2024-07-11",
+};
+console.log(post2.id);
+
 
 
 
