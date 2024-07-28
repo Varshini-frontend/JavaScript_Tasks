4b38adf92591de9afd4cf91877fa73f0d2d50bbc
Varshini-frontend <varshinianandaraj@gmail.com>
2024-07-28 20:58:11 +0530

    Ch6_pgm_08.js

--- 06_JS_Return_values/Ch6_pgm_08.js
+++ 06_JS_Return_values/Ch6_pgm_08.js
@@ -2,6 +2,7 @@
 
 var getPlayerHealth;
 var getPlayerPlace;
+var showPlayerInfo;
 
 getPlayerHealth = function (playerName, playerHealth) {
     return playerName + " has health " + playerHealth;
@@ -11,9 +12,10 @@ getPlayerPlace = function (playerName, playerPlace) {
     return playerName + " is in " + playerPlace;
 };
 
-console.log(getPlayerHealth("Kandra", 50));
-console.log(getPlayerPlace("Kandra", "The Dungeon of Doom"));
-
+showPlayerInfo = function (playerName, playerHealth, playerPlace) {
+    console.log(getPlayerHealth(playerName, playerHealth));
+    console.log(getPlayerPlace(playerName, playerPlace));
+};
 
 
 /* Further Adventures
