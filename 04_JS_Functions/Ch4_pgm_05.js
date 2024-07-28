685e5c7c23facaa51f017b9f8b5e5aa363816556
Varshini-frontend <varshinianandaraj@gmail.com>
2024-07-28 20:27:10 +0530

    Ch4_pgm_05.js

--- 04_JS_Functions/Ch4_pgm_05.js
+++ 04_JS_Functions/Ch4_pgm_05.js
@@ -1,18 +1,28 @@
 // Two more function definitions and assignments
 
-var findTotal;
-var displayMenu;
+var showSmaller = function (num1, num2) {
+    var smaller = Math.min(num1, num2);
+    var larger = Math.max(num1, num2);
 
-findTotal = function () {
-	result = number1 + number2;
+    console.log(smaller + " is smaller than " + larger);
 };
 
-displayMenu = function () {
-	console.log("Please choose an option:");
-	console.log("(1) Print log");
-	console.log("(2) Upload file");
-	console.log("(9) Quit");
+showSmaller(12, 3);
+showSmaller(-10, 3);
+showSmaller(3,0,8);
+
+var showLarger = function(num1, num2, num3){
+    var small = Math.min(num1, num2, num3);
+    var large1 = Math.max(num1, num2);
+    var large2 = Math.max(num2,num3);
+
+    console.log(small + " is smaller than " + large1 + " and " + large2);
 };
+
+showLarger(3,-9,7);
+showLarger(9,10,68);
+showLarger(4,0,-4);
+
 
 
 
