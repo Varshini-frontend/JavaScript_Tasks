d21099892aea8df83d34875068557022fd6fded3
Varshini-frontend <varshinianandaraj@gmail.com>
2024-07-28 20:29:18 +0530

    Ch4_pgm_08.js

--- 04_JS_Functions/Ch4_pgm_08.js
+++ 04_JS_Functions/Ch4_pgm_08.js
@@ -1,15 +1,16 @@
 // Displaying a menu
 
-var displayMenu;
+var message = "We choose to go to the Moon!";
 
-displayMenu = function () {
-	console.log("Please choose an option:");
-	console.log("(1) Print log");
-	console.log("(2) Upload file");
-	console.log("(9) Quit");
-};
+console.log(message.substr(3, 12));
+console.log(message.substr(20)); // Logs "Moon!"
+console.log(message.substr(11, 6)); // Logs "go to the"
+console.log(message.substr(0, 10));
+console.log(message.substr(3)); // Logs "choose to go to the Moon!"
+console.log(message.substr(-5)); // Logs "Moon!"
+console.log(message.substr(11, -3)); // Logs "go to the"
+// using message.substr(-5) starts from the 5th character from the end (Moon!). message.substr(11, -3) starts from index 11 and ignores the last 3 characters (go to the).
 
-displayMenu();
 
 
 
