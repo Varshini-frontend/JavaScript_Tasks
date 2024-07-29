d7c935a669e6f28d6438161b5e4998ff2a05e2c6
Varshini-frontend <varshinianandaraj@gmail.com>
2024-07-29 20:36:58 +0530

    Update Ch10_pgm_03.js

--- 10_JS_Conditions/Ch10_pgm_03.js
+++ 10_JS_Conditions/Ch10_pgm_03.js
@@ -1,18 +1,18 @@
 // Guess the number - using local scope
 
 var getGuesser = function () {
-    var secret = 8;
-  
-    return function (userNumber) {
-      if (userNumber === secret) {
-        console.log("Well done!");
-      } else {
-        console.log("Unlucky, try again.");
-      }
-    };
+  var secret = 8;
+
+  return function (userNumber) {
+    if (userNumber === secret) {
+      console.log("Well done!");
+    } else {
+      console.log("Unlucky, try again.");
+    }
   };
-  
-  var guess = getGuesser();
+};
+
+var guess = getGuesser();
   
   
   
